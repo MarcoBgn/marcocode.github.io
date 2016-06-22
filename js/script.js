d02630e5875d53d2aeaeff7ca8998e3d58d9810e
Marco Bagnasco <admin@hecticmojo.com>
2016-06-22 17:40:05 +0100

    Dropdown menu on event

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 jQuery(function ($) {
+    $('#contact').on('mouseenter', function (evt) {
+    var email = '<div class="dropdown" style="z-index: 1"><h4><a href="contact.html">E-mail</a></h4></div>';
+    var blog = '<div class="dropdown" style="z-index: 1"><h4><a id="blog" href="contact.html">Blog</a></h4></div>';
+    evt.preventDefault();
+    $(this).append('<hr class="drop-separator">', email, blog);
+    $('.dropdown').toggleClass('link-generated');
+  })
+
+  $('#contact').on('mouseleave', function (evt) {
+    evt.preventDefault();
+    $('.dropdown').remove();
+    $('.drop-separator').remove();
+  })
+
   $('#blog').on('click', function (evt) {
     evt.preventDefault();
-    $('.text-uppercase').first().append("<div class='notice'><h2>Coming Soon</h2></div>");
+    $('.text-uppercase').first().append("<div class='notice'><h2>Coming Soon!</h2></div>");
     setTimeout(function () {
       $('.notice').remove();
     }, 1500);
