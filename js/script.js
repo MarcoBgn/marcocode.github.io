eaa2f8239653ee9ff46cb6384fdd192b21fff260
Marco Bagnasco <admin@hecticmojo.com>
2016-06-22 18:33:36 +0100

    Event delegation - CSS transitions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 jQuery(function ($) {
-    $('#contact').mouseenter(function (evt) {
+    $('#more').mouseenter(function (evt) {
     var email = '<div class="dropdown" style="z-index: 1"><h4><a href="contact.html">E-mail</a></h4></div>';
-    var blog = '<div class="dropdown" style="z-index: 1"><h4><a id="blog" href="contact.html">Blog</a></h4></div>';
+    var blog = '<div class="dropdown" style="z-index: 1"><h4><a href="blog.html">Blog</a></h4></div>';
     evt.preventDefault();
     $(this).append('<hr class="drop-separator">', email, blog);
     $('.dropdown').toggleClass('link-generated');
   })
 
-  $('#contact').mouseleave(function (evt) {
+  $('#more').mouseleave(function (evt) {
     evt.preventDefault();
     $('.dropdown').remove();
     $('.drop-separator').remove();
+    $('.notice').toggleClass('trans');
   })
 
-  $('#blog').click(function (evt) {
+  $('#more').on('click', 'a[href^="blog"]', function (evt) {
     evt.preventDefault();
-    $('.text-uppercase').first().append("<div class='notice'><h2>Coming Soon!</h2></div>");
+    $('.navbar').last().append("<div class='notice'><h2>Coming Soon!</h2></div>");
     setTimeout(function () {
       $('.notice').remove();
-    }, 1500);
+    }, 1250);
+  })
+
+  $('#github').hover(function () {
+    setTimeout(function () {
+      $('.flash').remove();
+    }, 250);
+    $('.navbar').last().append("<div class='flash'><h2>~../> github/marcocode</h2></div>");
   })
 })
